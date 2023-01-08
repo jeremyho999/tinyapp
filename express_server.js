8dad77228b309acafd90d6bc9da1dc496fbcad36
Jeremy Ho <jeremyho999@gmail.com>
2023-01-07 21:20:38 -0700

    Create a URL submission form

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080;  // default port 8080
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -14,11 +15,8 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get('/urls/:id', (req, res) => {    // don't forget "/" before "urls/:id"!
-  //const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  const templateVars = { id, longURL };
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVars);
 });
 
