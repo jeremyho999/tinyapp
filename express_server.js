be1ca6926c80083ec28cea47f7a1143e36281564
Jeremy Ho <jeremyho999@gmail.com>
2023-01-07 21:27:54 -0700

    Add a GET route to show the URL submission form

--- express_server.js
+++ express_server.js
@@ -15,6 +15,11 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// add a GET route to show the form in "/views/urls_new.ejs"
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVars);
